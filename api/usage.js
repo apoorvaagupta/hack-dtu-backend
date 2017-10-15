7e6fb4bc1aaace2be4dd1e369026d9b397c50d3c
Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>
2017-10-15 12:25:08 +0530

    Refactor
    Signed-off-by: Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>

--- api/usage.js
+++ api/usage.js
@@ -5,56 +5,54 @@ const db = require('./../db/models');
 
 router.post('/', async (req, res) => {
 
-    db.models.Usage.create({
-        amount: +req.body.amount,
-        location: req.body.place,
-        // time: req.body.time,
-        usernfc: req.body.usernfc,
-        username: req.body.username,
-        usercontact: req.body.usercontact
-    }).then(async (usage) => {
-        let result = await BlockchainHandler.addTransaction({
-            amount: +req.body.amount,
-            location: req.body.place,
-            // time: req.body.time,
-            usernfc: req.body.usernfc,
-            username: req.body.username,
-            usercontact: req.body.usercontact
-        });
-        res.send({success: true, data: usage.get()})
-    }).catch((err) => {
-        console.log(err);
-        console.log('Internal Server Error');
-        res.send("Error");
-    })
+  db.models.Usage.create({
+    amount: +req.body.amount,
+    location: req.body.place,
+    usernfc: req.body.usernfc,
+    username: req.body.username,
+    usercontact: req.body.usercontact
+  }).then(async (usage) => {
+    let result = await BlockchainHandler.addTransaction({
+      amount: +req.body.amount,
+      location: req.body.place,
+      usernfc: req.body.usernfc,
+      username: req.body.username,
+      usercontact: req.body.usercontact
+    });
+    res.send({success: true, data: usage.get()})
+  }).catch((err) => {
+    console.log(err);
+    console.log('Internal Server Error');
+    res.send("Error");
+  })
 });
 
 router.get('/username/:username', (req, res) => {
-    db.models.BlockChain.findAll({
-        where: {username : req.params.username}
-    }).then((transactions) => {
-        if(transactions.length !== 0)
-        res.send({success: true, data: transactions});
-        else
-            res.send({success: false});
-    }).catch((e) => {
-        console.log(err);
-        res.send({success: false, error: e});
-    })
+  db.models.BlockChain.findAll({
+    where: {username: req.params.username}
+  }).then((transactions) => {
+    if (transactions.length !== 0)
+      res.send({success: true, data: transactions});
+    else
+      res.send({success: false});
+  }).catch((e) => {
+    console.log(err);
+    res.send({success: false, error: e});
+  })
 })
 
 router.get('/location/:location', (req, res) => {
-    db.models.BlockChain.findAll({
-        where: {location : req.params.location}
-    }).then((transactions) => {
-        if(transactions.length !== 0)
-            res.send({success: true, data: transactions});
-        else
-            res.send({success: false});
-    }).catch((e) => {
-        console.log(err);
-        res.send({success: false, error: e});
-    })
+  db.models.BlockChain.findAll({
+    where: {location: req.params.location}
+  }).then((transactions) => {
+    if (transactions.length !== 0)
+      res.send({success: true, data: transactions});
+    else
+      res.send({success: false});
+  }).catch((e) => {
+    console.log(err);
+    res.send({success: false, error: e});
+  })
 });
 
 module.exports = router;
